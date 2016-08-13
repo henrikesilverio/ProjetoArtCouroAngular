230c87d5fdda8f678f3bd08c0a7c170dde9fab68
henrique <henrikesilverio@gmail.com>
2016-08-13 19:05:29 -0300

    Inclusão do layout

---
+++ ProjetoArtCouro/Web/ProjetoArtCouro.Web/scripts/directives/chat/chat.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/**
+ * @ngdoc directive
+ * @name izzyposWebApp.directive:adminPosHeader
+ * @description
+ * # adminPosHeader
+ */
+angular.module("sbAdminApp")
+	.directive("chat", function () {
+	    return {
+	        templateUrl: "scripts/directives/chat/chat.html",
+	        restrict: "E",
+	        replace: true
+	    }
+	});
