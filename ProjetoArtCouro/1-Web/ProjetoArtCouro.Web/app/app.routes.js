20a467c608daa555ece8890ee7e709a3d98a0025
henrique <henrikesilverio@gmail.com>
2016-08-17 23:59:22 -0300

    Melhorias layout e inclusão dos menus

--- ProjetoArtCouro/1-Web/ProjetoArtCouro.Web/app/app.routes.js
+++ ProjetoArtCouro/1-Web/ProjetoArtCouro.Web/app/app.routes.js
@@ -45,46 +45,78 @@
 
         $stateProvider
             .state("root", {
-                abstract: true,
                 templateUrl: "app/shared/main/mainView.html",
                 resolve: {
                     loadScripts: loadScripts
                 }
+                //Home
             }).state("home", {
                 parent: "root",
                 url: "/home",
                 templateUrl: "app/components/home/views/homeView.html"
-            }).state("cadastro", {
-                abstract: true,
-                parent: "root"
+                //Cadastro
             }).state("cliente", {
-                parent: "cadastro",
-                url: "/cliente",
-                templateUrl: "app/components/cliente/views/clienteView.html"
+                parent: "root",
+                url: "/pesquisaClienteView",
+                templateUrl: "app/components/cliente/views/pesquisaClienteView.html"
             }).state("fornecedor", {
-                parent: "cadastro",
+                parent: "root",
                 url: "/fornecedor",
                 templateUrl: "app/components/fornecedor/views/fornecedorView.html"
             }).state("funcionario", {
-                parent: "cadastro",
+                parent: "root",
                 url: "/funcionario",
                 templateUrl: "app/components/funcionario/views/funcionarioView.html"
             }).state("produto", {
-                parent: "cadastro",
+                parent: "root",
                 url: "/produto",
                 templateUrl: "app/components/produto/views/produtoView.html"
             }).state("formaPagamento", {
-                parent: "cadastro",
+                parent: "root",
                 url: "/formaPagamento",
                 templateUrl: "app/components/formaPagamento/views/formaPagamentoView.html"
             }).state("condicaoPagamento", {
-                parent: "cadastro",
+                parent: "root",
                 url: "/condicaoPagamento",
                 templateUrl: "app/components/condicaoPagamento/views/condicaoPagamentoView.html"
             }).state("usuarios", {
-                parent: "cadastro",
+                parent: "root",
                 url: "/usuarios",
                 templateUrl: "app/components/usuario/views/usuarioView.html"
+                //Operações
+            }).state("compra", {
+                parent: "root",
+                url: "/compra",
+                templateUrl: "app/components/compra/views/compraView.html"
+            }).state("venda", {
+                parent: "root",
+                url: "/venda",
+                templateUrl: "app/components/venda/views/vendaView.html"
+            }).state("contaPagar", {
+                parent: "root",
+                url: "/contaPagar",
+                templateUrl: "app/components/contaPagar/views/contaPagarView.html"
+            }).state("contaReceber", {
+                parent: "root",
+                url: "/contaReceber",
+                templateUrl: "app/components/contaReceber/views/contaReceberView.html"
+                //Relatorios
+            }).state("estoque", {
+                parent: "root",
+                url: "/estoque",
+                templateUrl: "app/components/estoque/views/estoqueView.html"
+            }).state("grupoConfiguracao", {
+                parent: "root",
+                url: "/grupoConfiguracao",
+                templateUrl: "app/components/configuracao/views/grupoView.html"
+            }).state("configuracaoUsuario", {
+                parent: "root",
+                url: "/configuracaoUsuario",
+                templateUrl: "app/components/configuracao/views/configuracaoUsuarioView.html"
+            }).state("alterarSenha", {
+                parent: "root",
+                url: "/alterarSenha",
+                templateUrl: "app/components/usuario/views/alterarSenhaView.html"
             });
     }
 
