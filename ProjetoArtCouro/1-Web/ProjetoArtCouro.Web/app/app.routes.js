8ed8f4e107b572ab04d2266d53eae847ff2a6bea
henrique <henrikesilverio@gmail.com>
2016-08-21 15:52:22 -0300

    melhoria no layout do login e validações

--- ProjetoArtCouro/1-Web/ProjetoArtCouro.Web/app/app.routes.js
+++ ProjetoArtCouro/1-Web/ProjetoArtCouro.Web/app/app.routes.js
@@ -18,38 +18,43 @@
                         "app/shared/sidebar/sidebarSearch/sidebarSearchDirective.js"
                     ]
                 },
-                {
-                    name: "toggle-switch",
-                    files: [
-                        "bower_components/angular-toggle-switch/angular-toggle-switch.min.js",
-                        "bower_components/angular-toggle-switch/angular-toggle-switch.css"
-                    ]
-                }),
                 $ocLazyLoad.load(
                 {
                     name: "ngAnimate",
                     files: ["bower_components/angular-animate/angular-animate.js"]
-                }),
-                $ocLazyLoad.load(
-                {
-                    name: "ngCookies",
-                    files: ["bower_components/angular-cookies/angular-cookies.js"]
-                }),
-                $ocLazyLoad.load(
-                {
-                    name: "ngResource",
-                    files: ["bower_components/angular-resource/angular-resource.js"]
-                }),
-                $ocLazyLoad.load(
-                {
-                    name: "ngSanitize",
-                    files: ["bower_components/angular-sanitize/angular-sanitize.js"]
-                }),
-                $ocLazyLoad.load(
-                {
-                    name: "ngTouch",
-                    files: ["bower_components/angular-touch/angular-touch.js"]
-                });
+                }));
+            //{
+            //    name: "toggle-switch",
+            //    files: [
+            //        "bower_components/angular-toggle-switch/angular-toggle-switch.min.js",
+            //        "bower_components/angular-toggle-switch/angular-toggle-switch.css"
+            //    ]
+            //}),
+            //$ocLazyLoad.load(
+            //{
+            //    name: "ngAnimate",
+            //    files: ["bower_components/angular-animate/angular-animate.js"]
+            //}),
+            //$ocLazyLoad.load(
+            //{
+            //    name: "ngCookies",
+            //    files: ["bower_components/angular-cookies/angular-cookies.js"]
+            //}),
+            //$ocLazyLoad.load(
+            //{
+            //    name: "ngResource",
+            //    files: ["bower_components/angular-resource/angular-resource.js"]
+            //}),
+            //$ocLazyLoad.load(
+            //{
+            //    name: "ngSanitize",
+            //    files: ["bower_components/angular-sanitize/angular-sanitize.js"]
+            //}),
+            //$ocLazyLoad.load(
+            //{
+            //    name: "ngTouch",
+            //    files: ["bower_components/angular-touch/angular-touch.js"]
+            //});
         }
 
         $stateProvider
@@ -62,11 +67,11 @@
                 resolve: {
                     loadScripts: loadScripts
                 },
-                controller: function ($scope, $state, authService) {
+                controller: function ($scope, $location, authService) {
                     $scope.message = "";
                     $scope.logOut = function () {
                         authService.logOut();
-                        $state.go("auth.login");
+                        window.location = "/#/Login";
                     };
                 }
             //Login
