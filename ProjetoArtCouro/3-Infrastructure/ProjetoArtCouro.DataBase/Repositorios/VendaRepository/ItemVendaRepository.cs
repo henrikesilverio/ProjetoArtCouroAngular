502e0fc115588d63c7c7d0d7096a790c546033f5
henrique <henrikesilverio@gmail.com>
2018-12-03 21:08:32 -0200

    Inclusão dos teste de unidade para item de venda

--- ProjetoArtCouro/3-Infrastructure/ProjetoArtCouro.DataBase/Repositorios/VendaRepository/ItemVendaRepository.cs
+++ ProjetoArtCouro/3-Infrastructure/ProjetoArtCouro.DataBase/Repositorios/VendaRepository/ItemVendaRepository.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using ProjetoArtCouro.DataBase.DataBase;
+﻿using ProjetoArtCouro.DataBase.DataBase;
 using ProjetoArtCouro.Domain.Contracts.IRepository.IVenda;
 using ProjetoArtCouro.Domain.Entities.Vendas;
 
@@ -10,24 +7,10 @@ namespace ProjetoArtCouro.DataBase.Repositorios.VendaRepository
     public class ItemVendaRepository : IItemVendaRepository
     {
         private readonly DataBaseContext _context;
+
         public ItemVendaRepository(DataBaseContext context)
         {
             _context = context;
-        }
-
-        public ItemVenda ObterPorId(Guid id)
-        {
-            return _context.ItensVenda.FirstOrDefault(x => x.ItemVendaId.Equals(id));
-        }
-
-        public ItemVenda ObterPorCodigo(int codigo)
-        {
-            return _context.ItensVenda.FirstOrDefault(x => x.ItemVendaCodigo.Equals(codigo));
-        }
-
-        public List<ItemVenda> ObterLista()
-        {
-            return _context.ItensVenda.ToList();
         }
 
         public ItemVenda Criar(ItemVenda itemVenda)
@@ -35,12 +18,6 @@ namespace ProjetoArtCouro.DataBase.Repositorios.VendaRepository
             _context.ItensVenda.Add(itemVenda);
             _context.SaveChanges();
             return _context.Entry(itemVenda).Entity;
-        }
-
-        public void Atualizar(ItemVenda itemVenda)
-        {
-            _context.Entry(itemVenda).State = System.Data.Entity.EntityState.Modified;
-            _context.SaveChanges();
         }
 
         public void Deletar(ItemVenda itemVenda)
---
+++ ProjetoArtCouro/4-Test/ProjetoArtCouro.Test.DataBase/Vendas/ItemVendaRepositoryUnitTest.cs
@@ -0,0 +1,184 @@
+﻿using Effort;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjetoArtCouro.DataBase.DataBase;
+using ProjetoArtCouro.DataBase.Repositorios.VendaRepository;
+using ProjetoArtCouro.Domain.Entities.Pagamentos;
+using ProjetoArtCouro.Domain.Entities.Pessoas;
+using ProjetoArtCouro.Domain.Entities.Usuarios;
+using ProjetoArtCouro.Domain.Entities.Vendas;
+using ProjetoArtCouro.Domain.Models.Enums;
+using ProjetoArtCouro.Resources.Validation;
+using ProjetoArtCouro.Test.DataBase.Infra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjetoArtCouro.Test.DataBase.Vendas
+{
+    [TestClass]
+    public class ItemVendaRepositoryUnitTest
+    {
+        private DataBaseContext _context;
+
+        private Pessoa ObterPessoaBase()
+        {
+            return new Pessoa
+            {
+                Nome = "Henrique",
+                Papeis = new List<Papel>
+                {
+                    _context.Papeis
+                        .FirstOrDefault(x => x.PapelCodigo == (int)TipoPapelPessoaEnum.Cliente)
+                },
+                MeiosComunicacao = new List<MeioComunicacao>
+                {
+                    new MeioComunicacao
+                    {
+                        MeioComunicacaoNome = "henrikesilverio@gmail.com",
+                        TipoComunicacao = TipoComunicacaoEnum.Email,
+                        Principal = true
+                    }
+                },
+                Enderecos = new List<Endereco>
+                {
+                    new Endereco
+                    {
+                        Logradouro = "Rua A",
+                        Numero = "100",
+                        CEP = "88100566",
+                        Bairro = "Alfabeto",
+                        Cidade = "Maringá",
+                        Estado = new Estado { EstadoNome = "PR" },
+                        Principal = true
+                    }
+                },
+                PessoaFisica = new PessoaFisica
+                {
+                    CPF = "12345678909",
+                    RG = "203004009",
+                    Sexo = "M",
+                    EstadoCivil = new EstadoCivil { EstadoCivilNome = "Solteiro" }
+                }
+            };
+        }
+
+        private List<Permissao> ObterPermissoes()
+        {
+            return new List<Permissao>
+            {
+                new Permissao
+                {
+                    AcaoNome = "Pessoas",
+                    PermissaoNome = "Pessoas"
+                }
+            };
+        }
+
+        private GrupoPermissao ObterGrupoPermissao()
+        {
+            return new GrupoPermissao
+            {
+                GrupoPermissaoNome = "TODOS",
+                Permissoes = ObterPermissoes()
+            };
+        }
+
+        private Usuario ObterUsuarioBase()
+        {
+            return new Usuario
+            {
+                UsuarioNome = "Henrique",
+                Senha = PasswordAssertionConcern.Encrypt("123456"),
+                Ativo = true,
+                GrupoPermissao = ObterGrupoPermissao()
+            };
+        }
+
+        private Venda ObterVenda()
+        {
+            return new Venda
+            {
+                DataCadastro = DateTime.Now,
+                StatusVenda = StatusVendaEnum.Aberto,
+                ValorTotalBruto = 100,
+                ValorTotalDesconto = 0,
+                ValorTotalLiquido = 100,
+                Cliente = ObterPessoaBase(),
+                Usuario = ObterUsuarioBase(),
+                CondicaoPagamento = new CondicaoPagamento
+                {
+                    Ativo = true,
+                    Descricao = "A vista",
+                    QuantidadeParcelas = 1
+                },
+                FormaPagamento = new FormaPagamento
+                {
+                    Ativo = true,
+                    Descricao = "Cartão"
+                }
+            };
+        }
+
+        [TestInitialize]
+        public void Inicializacao()
+        {
+            var dbConnection = DbConnectionFactory.CreateTransient();
+            var modelBuilder = EntityFrameworkHelper.GetDbModelBuilder();
+            _context = new DataBaseContext(dbConnection, modelBuilder);
+        }
+
+        [TestMethod]
+        public void CriarItemVenda()
+        {
+            using (var repositorio = new ItemVendaRepository(_context))
+            {
+                repositorio.Criar(new ItemVenda
+                {
+                    PrecoVenda = 100,
+                    ProdutoCodigo = 1,
+                    ProdutoNome = "Cinto",
+                    Quantidade = 1,
+                    ValorBruto = 100,
+                    ValorDesconto = 0,
+                    ValorLiquido = 100,
+                    Venda = ObterVenda()
+                });
+
+                var itens = _context.ItensVenda.ToList();
+                Assert.IsTrue(itens.Any(), "Item não foi incluído");
+                Assert.IsTrue(itens.Any(x => x.PrecoVenda == 100), "Item não foi incluído");
+                Assert.IsTrue(itens.Any(x => x.ProdutoCodigo == 1), "Item não foi incluído");
+                Assert.IsTrue(itens.Any(x => x.ProdutoNome == "Cinto"), "Item não foi incluído");
+                Assert.IsTrue(itens.Any(x => x.Quantidade == 1), "Item não foi incluído");
+                Assert.IsTrue(itens.Any(x => x.ValorBruto == 100), "Item não foi incluído");
+                Assert.IsTrue(itens.Any(x => x.ValorDesconto == 0), "Item não foi incluído");
+                Assert.IsTrue(itens.Any(x => x.ValorLiquido == 100), "Item não foi incluído");
+            }
+        }
+
+        [TestMethod]
+        public void DeletarItemVenda()
+        {
+            using (var repositorio = new ItemVendaRepository(_context))
+            {
+                repositorio.Criar(new ItemVenda
+                {
+                    PrecoVenda = 100,
+                    ProdutoCodigo = 1,
+                    ProdutoNome = "Cinto",
+                    Quantidade = 1,
+                    ValorBruto = 100,
+                    ValorDesconto = 0,
+                    ValorLiquido = 100,
+                    Venda = ObterVenda()
+                });
+
+                var primeiro = _context.ItensVenda.FirstOrDefault();
+                Assert.IsNotNull(primeiro, "Item não foi incluído");
+                repositorio.Deletar(primeiro);
+                var retorno = _context.ItensVenda.FirstOrDefault();
+                Assert.IsNull(retorno, "Item não foi removido");
+            }
+        }
+    }
+}
